27114deff2b8c1820c1b41c3892e747b5b6a9da1
Mishka Sorochinskiy <mishash20161506@gmail.com>
2019-09-25 08:51:39 +0300

    added history endpoint

--- BookShop/BookShop/Controllers/AdminController.cs
+++ BookShop/BookShop/Controllers/AdminController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using BookShop.Application;
 using BookShop.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookShop.Controllers
@@ -13,10 +14,12 @@ namespace BookShop.Controllers
     public class AdminController : Controller
     {
         private readonly BookShopDb _context;
+        private readonly UserManager<IdentityUser> _manager;
 
-        public AdminController(BookShopDb context)
+        public AdminController(BookShopDb context,UserManager<IdentityUser> manager)
         {
             this._context = context;
+            this._manager = manager;
         }
 
         [HttpGet]
@@ -96,6 +99,36 @@ namespace BookShop.Controllers
                          ToList<GetOrderDto>();
                          
             return View(orders);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrderDetails(int id)
+        {
+            Order order = await this._context.Orders.FindAsync(id);
+
+            User user = (User)await this._manager.FindByIdAsync(order.UserId);
+
+            GetOrderDetailsDto dto = new GetOrderDetailsDto()
+            {
+                Email = user.Email
+            };
+
+            var books = (from i in this._context.Books
+                        where i.OrderId == order.Id
+                        select new BookDisplay()
+                        {
+                            Id = i.Id,
+                            Name = i.Name,
+                            Author = i.Author,
+                            Price = i.Price
+                        }).ToList<BookDisplay>();
+
+            double totalsum = books.Sum(b => b.Price);
+
+            dto.Books = books;
+            dto.TotalSum = totalsum;
+
+            return View(dto);
         }
 
         [HttpGet]
